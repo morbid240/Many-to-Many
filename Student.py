fe19bec5f5f696ea070d26a73bb080c61f86dced
morbid240 <162398707+morbid240@users.noreply.github.com>
2024-06-13 13:59:53 -0700

    Update Student.py

--- Student.py
+++ Student.py
@@ -1,10 +1,25 @@
+"""
+Malcolm Roddy
+CECS 323
+Many to Many SQL
+Due Date: 06/14/2024
+
+Only stuff added here: 
+    import Enrollment assoication class
+    new relationship between student and section
+    methods for adding/removing sections from the student
+    Everything else remains unchanged (I think)
+"""
+
+
 from orm_base import Base
 from sqlalchemy import Column, Integer, UniqueConstraint, Identity, String
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 from typing import List
 from StudentMajor import StudentMajor
-from Enrollment import Enrollment
 from datetime import datetime
+
+from Enrollment import Enrollment
 
 class Student(Base):
     """An individual who may or may not be enrolled at the university, who
@@ -12,10 +27,11 @@ class Student(Base):
     could be a formal degree program, or it could be a specialized certificate."""
     __tablename__ = "students"
     # Let SQLAlchemy handle the generation of student_id values for us.
-    student_id: Mapped[int] = mapped_column(Integer, Identity(start=1, cycle=True), primary_key=True)
-    last_name: Mapped[str] = mapped_column(String(50), nullable=False)
-    first_name: Mapped[str] = mapped_column(String(50), nullable=False)
-    email: Mapped[str] = mapped_column(String(255), nullable=False)
+    studentID: Mapped[int] = mapped_column('student_id', Integer, Identity(start=1, cycle=True), primary_key=True)
+    lastName: Mapped[str] = mapped_column('last_name', String(50), nullable=False, primary_key=False)
+    firstName: Mapped[str] = mapped_column('first_name', String(50), nullable=False, primary_key=False)
+    email: Mapped[str] = mapped_column('email', String(255), nullable=False)
+    
     # Relationships
     majors: Mapped[List["StudentMajor"]] = relationship(
         "StudentMajor", back_populates="student", cascade="all, save-update, delete-orphan"
@@ -23,18 +39,35 @@ class Student(Base):
     sections: Mapped[List["Enrollment"]] = relationship(
         "Enrollment", back_populates="student", cascade="all, save-update, delete-orphan"
     )
-    # __table_args__ can best be viewed as directives that we ask SQLAlchemy to
-    # send to the database. In this case, we want two separate uniqueness constraints (candidate keys).
+
+    #Constraints
     __table_args__ = (
         UniqueConstraint("first_name", "last_name", name="students_uk_01"),
         UniqueConstraint("email", name="students_uk_02")
     )
 
-    
-    def __init__(self, last_name: str, first_name: str, email: str):
-        self.last_name = last_name
-        self.first_name = first_name
+    def __init__(self, lastName: str, firstName: str, email: str):
+        self.lastName = lastName
+        self.firstName = firstName
         self.email = email
+
+    # add/remove sections in student list
+    def add_section(self, section):
+        """Add a section to a list of sections the student is enrolled in currently."""
+        for next_section in self.sections:
+            if next_section.section == section:
+                return  # Student already enrolled in section
+        student_enrollment = Enrollment(section, self)
+        section.students.append(student_enrollment)
+        self.sections.append(student_enrollment)
+
+    
+    def remove_section(self, section):
+        """Remove a section from the list of sections the student is enrolled in."""
+        for next_section in self.sections:
+            if next_section.section == section:
+                self.sections.remove(next_section)
+                return
 
     
     def add_major(self, major):
@@ -51,24 +84,6 @@ class Student(Base):
         for next_major in self.majors:
             if next_major.major == major:
                 self.majors.remove(next_major)
-                return
-
-    
-    def add_section(self, section):
-        """Add a section to a list of sections the student is enrolled in currently."""
-        for next_section in self.sections:
-            if next_section.section == section:
-                return  # Student already enrolled in section
-        student_enrollment = Enrollment(section, self)
-        section.students.append(student_enrollment)
-        self.sections.append(student_enrollment)
-
-    
-    def remove_section(self, section):
-        """Remove a section from the list of sections the student is enrolled in."""
-        for next_section in self.sections:
-            if next_section.section == section:
-                self.sections.remove(next_section)
                 return
 
     
