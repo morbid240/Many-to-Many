b2080f938c4facb7fbcc530dca0a08bd2c9bac89
morbid240 <162398707+morbid240@users.noreply.github.com>
2024-06-13 16:56:58 -0700

    updated from sample

--- Student.py
+++ Student.py
@@ -12,13 +12,14 @@ Everything else remains unchanged (I think)
 """
 
 from orm_base import Base
-from sqlalchemy import Column, Integer, UniqueConstraint, Identity, String
+from sqlalchemy import Column, Integer, UniqueConstraint, Identity
+from sqlalchemy import String
 from sqlalchemy.orm import Mapped, mapped_column, relationship
-from typing import List
+from typing import List                 # Use this for the list of Majors that this student has
 from StudentMajor import StudentMajor
+from Enrollment import Enrollment
 from datetime import datetime
 
-from Enrollment import Enrollment
 
 class Student(Base):
     """An individual who may or may not be enrolled at the university, who
@@ -30,7 +31,6 @@ class Student(Base):
     lastName: Mapped[str] = mapped_column('last_name', String(50), nullable=False, primary_key=False)
     firstName: Mapped[str] = mapped_column('first_name', String(50), nullable=False, primary_key=False)
     email: Mapped[str] = mapped_column('email', String(255), nullable=False)
-    
     # Relationships
     majors: Mapped[List["StudentMajor"]] = relationship(
         "StudentMajor", back_populates="student", cascade="all, save-update, delete-orphan"
@@ -38,17 +38,18 @@ class Student(Base):
     sections: Mapped[List["Enrollment"]] = relationship(
         "Enrollment", back_populates="student", cascade="all, save-update, delete-orphan"
     )
-
     #Constraints
     __table_args__ = (
         UniqueConstraint("first_name", "last_name", name="students_uk_01"),
         UniqueConstraint("email", name="students_uk_02")
     )
 
+
     def __init__(self, lastName: str, firstName: str, email: str):
         self.lastName = lastName
         self.firstName = firstName
         self.email = email
+
 
     # add/remove sections in student list
     def add_section(self, section):
@@ -68,23 +69,35 @@ class Student(Base):
                 self.sections.remove(next_section)
                 return
 
-    
+
     def add_major(self, major):
-        """Add a new major to the student."""
+        """Add a new major to the student.  We are not actually adding a major directly
+        to the student.  Rather, we are adding an instance of StudentMajor to the student.
+        :param  major:  The Major that this student has declared.
+        :return:        None
+        """
+        # Make sure that this student does not already have this major.
         for next_major in self.majors:
             if next_major.major == major:
-                return  # This student already has this major
+                return                  # This student already has this major
+        # Create the new instance of StudentMajor to connect this Student to the supplied Major.
         student_major = StudentMajor(self, major, datetime.now())
-        self.majors.append(student_major)
+#        major.students.append(student_major)                # Add this Student to the supplied Major.
+#        self.majors.append(student_major)                   # Add the supplied Major to this student.
 
-    
     def remove_major(self, major):
-        """Remove a major from the list of majors that a student presently has declared."""
+        """
+        Remove a major from the list of majors that a student presently has declared.
+        Essentially, we are UNdeclaring the major.  A bit contrived, but this is for
+        demonstration purposes.
+        :param major:
+        :return:
+        """
         for next_major in self.majors:
+            # This item in the list is the major we are looking for for this student.
             if next_major.major == major:
                 self.majors.remove(next_major)
                 return
 
-    
     def __str__(self):
-        return f"Student ID: {self.student_id} name: {self.last_name}, {self.first_name} e-mail: {self.email}"
+        return f"Student ID: {self.studentID} name: {self.lastName}, {self.firstName} e-mail: {self.email}"
