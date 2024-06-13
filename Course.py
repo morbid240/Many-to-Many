d6755644c029b3fa797d443b4ebc25101717d898
morbid240 <162398707+morbid240@users.noreply.github.com>
2024-06-13 14:34:30 -0700

    Update Course.py

--- Course.py
+++ Course.py
@@ -1,9 +1,19 @@
+"""
+Malcolm Roddy
+CECS 323 S01
+Many to Many 
+Due Date: 06/14/2024
+
+The only thing added here is the relationship of course to
+sections. Organized a tad but to see where attributes and constraints are
+"""
+
 from orm_base import Base
 from sqlalchemy import Integer, UniqueConstraint, ForeignKeyConstraint
 from sqlalchemy import String, Integer
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 from Department import Department
-from typing import List    # to get clist of sections
+from typing import List # get a list of sections
 
 class Course(Base):
     """A catalog entry.  Each course proposes to offer students who enroll in
@@ -21,59 +31,44 @@ class Course(Base):
     ForeignKey.  I show you how to do it in __table_args__ because you'll need
     that for the relationship from courses into sections.
     """
-    # Primary keys
-    departmentAbbreviation: Mapped[str] = mapped_column('department_abbreviation', primary_key=True)
-    courseNumber: Mapped[int] = mapped_column('course_number', Integer, nullable=False, primary_key=True)
-    # Basic attributes
+    # Primary Keys
+    departmentAbbreviation: Mapped[str] = mapped_column('department_abbreviation',
+                                                        primary_key=True)
+    courseNumber: Mapped[int] = mapped_column('course_number', Integer,
+                                              nullable=False, primary_key=True)
+    # Other attributes
     name: Mapped[str] = mapped_column('name', String(50), nullable=False)
     description: Mapped[str] = mapped_column('description', String(500), nullable=False)
     units: Mapped[int] = mapped_column('units', Integer, nullable=False)
-    '''
-    relationship to department: many to one, we map course to one department
-    relatiosnhip to section: one to many where we keep track of a list of sections
-    for every course. '''
+
     # Relationships
-    department: Mapped["Department"] = relationship(back_populates="courses") # many to one
+    department: Mapped["Department"] = relationship(back_populates="courses") # relating child to parent
     sections: Mapped[List["Section"]] = relationship(back_populates="course") # one to many
-    # Uniquness constraints, foreign keys etc
-    __table_args__ = (UniqueConstraint("department_abbreviation", "name", name="courses_uk_01"),
-                      ForeignKeyConstraint([departmentAbbreviation],
-                                           [Department.abbreviation]))
+    # __table_args__ can best be viewed as directives that we ask SQLAlchemy to
+    # send to the database.  In this case, that we want two separate uniqueness
+    # constraints (candidate keys).
+    __table_args__ = (
+        UniqueConstraint("department_abbreviation", "name", name="courses_uk_01"),
+        ForeignKeyConstraint([departmentAbbreviation],[Department.abbreviation])
+    )
+
     def __init__(self, department: Department, courseNumber: int, name: str, description: str, units: int):
-            self.init(department, courseNumber, name, description, units)
+        self.set_department(department)
+        self.courseNumber = courseNumber
+        self.name = name
+        self.description = description
+        self.units = units
 
-'''
-    Setting up to make sure dependent tables are defined 
-'''
-def set_department(self, department: Department):
-    """
-    Accept a new department without checking for any uniqueness.
-    I'm going to assume that either a) the caller checked that first
-    and/or b) the database will raise its own exception.
-    :param department:  The new department for the course.
-    :return:            None
-    """
-    self.department = department
-    self.departmentAbbreviation = department.abbreviation
+    def set_department(self, department: Department):
+        """
+        Accept a new department withoug checking for any uniqueness.
+        I'm going to assume that either a) the caller checked that first
+        and/or b) the database will raise its own exception.
+        :param department:  The new department for the course.
+        :return:            None
+        """
+        self.department = department
+        self.departmentAbbreviation = department.abbreviation
 
-
-def init(self, department: Department, courseNumber: int, name: str, description: str, units: int):
-    self.set_department(department)
-    self.courseNumber = courseNumber
-    self.name = name
-    self.description = description
-    self.units = units
-
-
-def __str__(self):
-    return f"\n{self.departmentAbbreviation} {self.courseNumber} \n\t{self.name} \tunits: {self.units}\n"
-
-
-"""Add the two instance methods to the class, regardless of whether we introspect or not."""
-setattr(Course, 'init', init)
-setattr(Course, 'set_department', set_department)
-setattr(Course, '__str__', __str__)
-
-
-
-
+    def __str__(self):
+        return f"Department abbrev: {self.departmentAbbreviation} number: {self.courseNumber} name: {self.name} units: {self.units}"
