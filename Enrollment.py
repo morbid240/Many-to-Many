9c7521a5dc65fcd177c61b4c383b1af92cc1a4b6
morbid240 <162398707+morbid240@users.noreply.github.com>
2024-06-13 00:17:57 -0700

    Update Enrollment.py

--- Enrollment.py
+++ Enrollment.py
@@ -7,9 +7,9 @@ from typing import List
 
 
 class Enrollment(Base):
+    """Association class between students and sections."""
     __tablename__ = "enrollments"  # Give SQLAlchemy th name of the table.
     # Primary keys - all migrating foreign keys I guess...
-    """Primary keys:{student_id, department_abbreviation, course_number, section_number, section_year, semester} """
     studentID: Mapped[int}: Mapped[int] = mapped_column('student_id', primary_key=True)
     departmentAbbreviation: Mapped[str] = mapped_column('department_abbreviation', primary_key=True)
     courseNumber: Mapped[int] = mapped_column('course_number', primary_key=True)
@@ -17,19 +17,18 @@ class Enrollment(Base):
     semester: Mapped[str] = mapped_column('semester', String(10), nullable=False, primary_key=True)
     sectionYear: Mapped[int] = mapped_column('section_year', Integer, nullable=False, primary_key=True)
     # Define relationships 
-    students: Mapped[List["Student"]] = relationship(back_populates="enrollments")  
-    sections: Mapped[List["Section"]] = relationship(back_populates="enrollments") 
-
+    section: Mapped["Section"] = relationship(back_populates="students")  
+    student: Mapped["Student"] = relationship(back_populates="sections") 
     # FK constraint makes sure that no student enrolls in the same course more than once the same semester
     __table_args__ = (
         ForeignKeyConstraint([departmentAbbreviation, courseNumber, sectionYear, semester, studentID], 
-                             [Section.departmentAbbreviation, Section.courseNumber, Section.semester, Student.studentID])
+                             [section.departmentAbbreviation, section.courseNumber, section.semester, student.studentID])
     )
 
-    def __init__(self, section: Section, student: Student):
+    def __init__(self, section, student):
         self.set_section(section)
         self.set_student(section)
-            
+    # get stuff from classes
     def set_student(self, student):
         self.student = student
         self.studentID = student.studentID
@@ -40,3 +39,6 @@ class Enrollment(Base):
         self.sectionNumber = section.sectionNumber
         self.semester = section.semester
         self.sectionYear = section.sectionYear
+
+    def __str__(self):
+        return f"Student {self.student} enrolled in section: {self.section}"
