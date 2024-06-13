8e12adc61fdd1084e52afca26f31407773875697
morbid240 <162398707+morbid240@users.noreply.github.com>
2024-06-13 12:37:40 -0700

    Update QueryAdd.py

--- QueryAdd.py
+++ QueryAdd.py
@@ -232,6 +232,31 @@ def add_student_section(session: Session):
     session.add_student(student)
     session.flush()
 
+
+def boilerplate(sess: Session):
+    """
+    Add boilerplate data initially to jump start the testing.  Remember that there is no
+    checking of this data, so only run this option once from the console, or you will
+    get a uniqueness constraint violation from the database.
+    :param sess:    The session that's open.
+    :return:        None
+    """
+    department: Department = Department('CECS', 'Computer Engineering Computer Science')
+    major1: Major = Major(department, 'Computer Science', 'Fun with blinking lights')
+    major2: Major = Major(department, 'Computer Engineering', 'Much closer to the silicon')
+    student1: Student = Student('Brown', 'David', 'david.brown@gmail.com')
+    student2: Student = Student('Brown', 'Mary', 'marydenni.brown@gmail.com')
+    student3: Student = Student('Disposable', 'Bandit', 'disposable.bandit@gmail.com')
+    student1.add_major(major1)
+    student2.add_major(major1)
+    student2.add_major(major2)
+    sess.add(department)
+    sess.add(major1)
+    sess.add(major2)
+    sess.add(student1)
+    sess.add(student2)
+    sess.add(student3)
+    sess.flush()    
         
         
 
