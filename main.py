ae04c81cfa130de8c3f50cd6be20d5b156fea716
morbid240 <162398707+morbid240@users.noreply.github.com>
2024-06-12 19:56:57 -0700

    Update main.py

--- main.py
+++ main.py
@@ -20,7 +20,62 @@ from QueryDelete import delete_course, delete_department, delete_major_student, 
 from QueryList import list_course, list_department, list_department_courses, list_major, list_major_student, list_student, list_student_major
 
 from DatabaseManager import add, delete, list_objects, boilerplate, session_rollback
+from pprint import pprint
 
+from QueryAdd import add_major
+from QueryAdd import add_department, add_course, add_major, add_section, add_major_student, add_student, add_student_major
+from QuerySelect import select_course, select_department, select_major, select_section, select_student, select_student_from_list
+from QueryDelete import delete_course, delete_department, delete_major_student, delete_section, delete_student, delete_student_major
+from QueryList import list_course, list_department, list_department_courses, list_major, list_major_student, list_student, list_student_major
+
+
+def add(sess: Session):
+    add_action: str = ''
+    while add_action != add_menu.last_action():
+        add_action = add_menu.menu_prompt()
+        exec(add_action)
+
+
+def delete(sess: Session):
+    delete_action: str = ''
+    while delete_action != delete_menu.last_action():
+        delete_action = delete_menu.menu_prompt()
+        exec(delete_action)
+
+
+def list_objects(sess: Session):
+    list_action: str = ''
+    while list_action != list_menu.last_action():
+        list_action = list_menu.menu_prompt()
+        exec(list_action)
+
+
+
+def boilerplate(sess: Session):
+    """
+    Add boilerplate data initially to jump start the testing.  Remember that there is no
+    checking of this data, so only run this option once from the console, or you will
+    get a uniqueness constraint violation from the database.
+    :param sess:    The session that's open.
+    :return:        None
+    """
+    department: Department = Department('CECS', 'Computer Engineering Computer Science')
+    major1: Major = Major(department, 'Computer Science', 'Fun with blinking lights')
+    major2: Major = Major(department, 'Computer Engineering', 'Much closer to the silicon')
+    student1: Student = Student('Brown', 'David', 'david.brown@gmail.com')
+    student2: Student = Student('Brown', 'Mary', 'marydenni.brown@gmail.com')
+    student3: Student = Student('Disposable', 'Bandit', 'disposable.bandit@gmail.com')
+    student1.add_major(major1)
+    student2.add_major(major1)
+    student2.add_major(major2)
+    sess.add(department)
+    sess.add(major1)
+    sess.add(major2)
+    sess.add(student1)
+    sess.add(student2)
+    sess.add(student3)
+    sess.flush()    
+    
 
 if __name__ == '__main__':
     print('Starting off')
