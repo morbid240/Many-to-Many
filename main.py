6d3a873956a231a0ec701e3451901d4b23a1468c
morbid240 <162398707+morbid240@users.noreply.github.com>
2024-06-12 17:57:09 -0700

    Update main.py

--- main.py
+++ main.py
@@ -4,14 +4,16 @@ from menu_definitions import menu_main, add_menu, delete_menu, list_menu, debug_
 from IntrospectionFactory import IntrospectionFactory
 from db_connection import engine, Session
 from orm_base import metadata
-# Note that until you import your SQLAlchemy declarative classes, such as Student, Python
-# will not execute that code, and SQLAlchemy will be unaware of the mapped table.
-from Department import Department
-from Course import Course
-from Section import Section
+# just getting functions from modules
+from QueryDepartment import add_department, select_department, delete_department, list_departments, list_department_courses
+from QueryCourse import add_course, select_course, delete_course, list_courses, move_course_to_new_department
+from QuerySection import add_section, select_section, delete_section
+
+
 from Major import Major
 from Student import Student
 from StudentMajor import StudentMajor
+
 from Option import Option
 from Menu import Menu
 from SQLAlchemyUtilities import check_unique
@@ -72,34 +74,6 @@ def add_major(session: Session):
     session.add(major)
 
 
-def add_student(session: Session):
-    """
-    Prompt the user for the information for a new student and validate
-    the input to make sure that we do not create any duplicates.
-    :param session: The connection to the database.
-    :return:        None
-    """
-    unique_name: bool = False
-    unique_email: bool = False
-    last_name: str = ''
-    first_name: str = ''
-    email: str = ''
-    while not unique_email or not unique_name:
-        last_name = input("Student last name--> ")
-        first_name = input("Student first name-->")
-        email = input("Student e-mail address--> ")
-        name_count: int = session.query(Student).filter(Student.lastName == last_name,
-                                                        Student.firstName == first_name).count()
-        unique_name = name_count == 0
-        if not unique_name:
-            print("We already have a student by that name.  Try again.")
-        if unique_name:
-            email_count = session.query(Student).filter(Student.email == email).count()
-            unique_email = email_count == 0
-            if not unique_email:
-                print("We already have a student with that email address.  Try again.")
-    new_student = Student(last_name, first_name, email)
-    session.add(new_student)
 
 
 def add_student_major(sess):
@@ -156,26 +130,7 @@ def add_major_student(sess):
 
 
 
-def select_student(sess) -> Student:
-    """
-    Select a student by the combination of the last and first.
-    :param sess:    The connection to the database.
-    :return:        The selected student.
-    """
-    found: bool = False
-    last_name: str = ''
-    first_name: str = ''
-    while not found:
-        last_name = input("Student's last name--> ")
-        first_name = input("Student's first name--> ")
-        name_count: int = sess.query(Student).filter(Student.lastName == last_name,
-                                                     Student.firstName == first_name).count()
-        found = name_count == 1
-        if not found:
-            print("No student found by that name.  Try again.")
-    student: Student = sess.query(Student).filter(Student.lastName == last_name,
-                                                  Student.firstName == first_name).first()
-    return student
+
 
 
 def select_major(sess) -> Major:
@@ -196,22 +151,6 @@ def select_major(sess) -> Major:
     return major
 
 
-
-def delete_student(session: Session):
-    """
-    Prompt the user for a student to delete and delete them.
-    :param session:     The current connection to the database.
-    :return:            None
-    """
-    student: Student = select_student(session)
-    """This is a bit ghetto.  The relationship from Student to StudentMajor has 
-    cascade delete, so this delete will work even if a student has declared one
-    or more majors.  I could write a method on Student that would return some
-    indication of whether it has any children, and use that to let the user know
-    that they cannot delete this particular student.  But I'm too lazy at this
-    point.
-    """
-    session.delete(student)
 
 
 def delete_student_major(sess):
@@ -237,16 +176,6 @@ def delete_major_student(sess):
 
 
 
-def list_student(sess: Session):
-    """
-    List all Students currently in the database.
-    :param sess:    The current connection to the database.
-    :return:
-    """
-    students: [Student] = list(sess.query(Student).order_by(Student.lastName, Student.firstName))
-    for student in students:
-        print(student)
-
 
 def list_major(sess: Session):
     """
@@ -258,19 +187,6 @@ def list_major(sess: Session):
     for major in majors:
         print(major)
 
-
-def list_student_major(sess: Session):
-    """Prompt the user for the student, and then list the majors that the student has declared.
-    :param sess:    The connection to the database
-    :return:        None
-    """
-    student: Student = select_student(sess)
-    recs = sess.query(Student).join(StudentMajor, Student.studentID == StudentMajor.studentId).join(
-        Major, StudentMajor.majorName == Major.name).filter(
-        Student.studentID == student.studentID).add_columns(
-        Student.lastName, Student.firstName, Major.description, Major.name).all()
-    for stu in recs:
-        print(f"Student name: {stu.lastName}, {stu.firstName}, Major: {stu.name}, Description: {stu.description}")
 
 
 def list_major_student(sess: Session):
@@ -290,33 +206,6 @@ def list_major_student(sess: Session):
 
 
 
-def select_student_from_list(session):
-    """
-    This is just a cute little use of the Menu object.  Basically, I create a
-    menu on the fly from data selected from the database, and then use the
-    menu_prompt method on Menu to display characteristic descriptive data, with
-    an index printed out with each entry, and prompt the user until they select
-    one of the Students.
-    :param session:     The connection to the database.
-    :return:            None
-    """
-    # query returns an iterator of Student objects, I want to put those into a list.  Technically,
-    # that was not necessary, I could have just iterated through the query output directly.
-    students: [Department] = list(sess.query(Department).order_by(Department.lastName, Department.firstName))
-    options: [Option] = []  # The list of menu options that we're constructing.
-    for student in students:
-        # Each time we construct an Option instance, we put the full name of the student into
-        # the "prompt" and then the student ID (albeit as a string) in as the "action".
-        options.append(Option(student.lastName + ', ' + student.firstName, student.studentId))
-    temp_menu = Menu('Student list', 'Select a student from this list', options)
-    # text_studentId is the "action" corresponding to the student that the user selected.
-    text_studentId: str = temp_menu.menu_prompt()
-    # get that student by selecting based on the int version of the student id corresponding
-    # to the student that the user selected.
-    returned_student = sess.query(Department).filter(Department.studentId == int(text_studentId)).first()
-    # this is really just to prove the point.  Ideally, we would return the student, but that
-    # will present challenges in the exec call, so I didn't bother.
-    print("Selected student: ", returned_student)
 
 
 
