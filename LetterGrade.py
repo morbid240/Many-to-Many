dc41464a90312e39d0ed1d7b4a0bd650a4372fc4
morbid240 <162398707+morbid240@users.noreply.github.com>
2024-06-24 01:59:10 -0700

    Update LetterGrade.py

--- LetterGrade.py
+++ LetterGrade.py
@@ -11,7 +11,7 @@ Due Date: 06/23/2024
 from sqlalchemy import Date, ForeignKey, String, UniqueConstraint, CheckConstraint
 from sqlalchemy.orm import mapped_column, Mapped, relationship
 
-
+from Enrollment import Enrollment
 
 
 
@@ -36,13 +36,9 @@ class LetterGrade(Enrollment):
 
     # Constraints
     __table_args__ = (
-        CheckConstraint(min_satisfactory.in_(['A', 'B', 'C', 'D', 'F']), name='letter_grade_ck_min_satisfactory'),
+        CheckConstraint(min_satisfactory.in_(['A', 'B', 'C', 'D', 'F']), name='letter_grade_uk_01'),
     )
 
-    """This is what makes it defined as inheritable"""
-    __mapper_args__ = {
-        "polymorphic_identity": "letter_grade"
-    }
 
     # Constructor
     def __init__(self, section, student, min_satisfactory: str, grade: str):
